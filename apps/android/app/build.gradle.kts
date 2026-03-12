3ab3650d6d3edb934975ab25e8092bf83393238d
ingomc <janick2002@gmx.de>
2026-03-12 21:35:41 +0100

    release: v0.23.0

--- apps/android/app/build.gradle.kts
+++ apps/android/app/build.gradle.kts
@@ -46,8 +46,8 @@ android {
         applicationId = "de.ingomc.nozio"
         minSdk = 26
         targetSdk = 35
-        versionCode = 28
-        versionName = "0.22.0"
+        versionCode = 29
+        versionName = "0.23.0"
         buildConfigField("String", "FOOD_API_BASE_URL", "\"${localProps.getProperty("FOOD_API_BASE_URL", "http://10.0.2.2:3000/")}\"")
         buildConfigField("String", "FOOD_API_KEY", "\"${localProps.getProperty("FOOD_API_KEY", "dev-change-me")}\"")
         buildConfigField("String", "GOOGLE_WEB_CLIENT_ID", "\"${propOrEnv("GOOGLE_WEB_CLIENT_ID") ?: "dev-change-me"}\"")
